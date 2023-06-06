3bc9d06992dfe2be22e0cd2d2596983253928df6
Sandro Kvrgic <sandro.kvrgic@ru.nl>
2023-06-06 12:44:24 +0200

    level 1

---
+++ Assets/Scripts/Managers/QuitGameManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitGameManager : MonoBehaviour
+{
+     public void ButtonQuitClicked(){
+        Application.Quit();
+
+    }
+
+}
---
+++ Assets/Scripts/Managers/RestartManager.cs
@@ -0,0 +1,14 @@
+﻿ using System.Collections;
+ using System.Collections.Generic;
+ using UnityEngine;
+
+ public class RestartManager : MonoBehaviour
+ {
+     [SerializeField] private GameObject levelManager;
+     [SerializeField] private GameObject gameOverMenu;
+    public void ButtonRestartClicked(){
+         LevelManager lm = levelManager.GetComponent<LevelManager>();
+        //  lm.RestartGame();
+         gameOverMenu.SetActive(false);
+     }
+ }
